7cf30071738d8c0861e37684f4cdf0fad643304d
AlekseiDevel <aleksei1982@atomicmail.io>
2026-02-27 01:11:52 +0300

    feat: add database schema using SQLAlchemy and requirements

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import os
-import datetime
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+import datetime
 
 Base = declarative_base()
+
 
 class RequestLog(Base):
     __tablename__ = 'requests_log'
@@ -12,7 +13,9 @@ class RequestLog(Base):
     timestamp = Column(DateTime, default=datetime.datetime.utcnow)
     city = Column(String)
     status_code = Column(Integer)
+
     weather_entries = relationship("WeatherData", back_populates="request")
+
 
 class WeatherData(Base):
     __tablename__ = 'weather_data'
@@ -21,10 +24,14 @@ class WeatherData(Base):
     temperature = Column(Float)
     humidity = Column(Integer)
     description = Column(String)
+
     request = relationship("RequestLog", back_populates="weather_entries")
 
-engine = create_engine(os.getenv('DATABASE_URL'))
+
+# Получаем URL из .env. Для теста можно заменить на 'sqlite:///./test.db'
+engine = create_engine(os.getenv('DATABASE_URL', 'sqlite:///./test.db'))
 SessionLocal = sessionmaker(bind=engine)
+
 
 def init_db():
     Base.metadata.create_all(bind=engine)
